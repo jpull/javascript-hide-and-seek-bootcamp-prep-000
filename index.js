4f64c20fbf3a921bd4edb77a4d562cffa302cc48
Jerry P <jpull@jpull.com>
2018-02-16 01:33:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
 function nestedTarget() {
-  return document.querySelector('.target');
+  return document.getElementById('nested')document.querySelector('.target');
 }
