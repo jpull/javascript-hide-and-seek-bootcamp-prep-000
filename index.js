72c4e3db5a693bce4f6846d34adad18da3db8a35
Jerry P <jpull@jpull.com>
2018-02-16 01:36:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ function nestedTarget() {
   return document.querySelector('#nested .target')
 }
 function increaseRankBy(n) {
-  
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (i + n).toString()
+  }
 }
