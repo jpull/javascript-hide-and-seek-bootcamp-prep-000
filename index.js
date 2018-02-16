da8c4eafe51e1843730519ea64282ffc1e6e3420
Jerry P <jpull@jpull.com>
2018-02-16 01:39:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function nestedTarget() {
   return document.querySelector('#nested .target')
 }
 function increaseRankBy(n) {
-  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  const lis = document.querySelectorAll('.ranked-list')
 
   for (let i = 0; i < lis.length; i++) {
     lis[i].innerHTML = (i + n).toString()
